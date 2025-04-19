a1b202a34a899d6c9ffe3414e00a7d93af147294
Silas Stilling <sis002@edu.zealand.dk>
2025-04-19 03:28:55 +0200

    Tilføjet logik angående hvad en Creature kan gøre

--- GameFrameworkLibrary/Models/Creatures/Creature.cs
+++ GameFrameworkLibrary/Models/Creatures/Creature.cs
@@ -5,6 +5,7 @@ using GameFrameworkLibrary.Models.Environment;
 using GameFrameworkLibrary.Models.ItemObjects;
 using System.Diagnostics;
 using System.ComponentModel;
+using System.Runtime.Intrinsics.Arm;
 
 namespace GameFrameworkLibrary.Models.Creatures
 {
@@ -16,6 +17,7 @@ namespace GameFrameworkLibrary.Models.Creatures
         private readonly List<AttackItem> _attackItems = new();
         private readonly List<DefenceItem> _defenceItems = new();
         private readonly ILogger _logger;
+        private readonly List<IUsable> _usables = new();
 
         public Creature(string name, string? description, int hitpoints, Position startPosition, ILogger logger)
             : base(name, description)
@@ -27,6 +29,18 @@ namespace GameFrameworkLibrary.Models.Creatures
         public override string ToString() =>
             $"(HP: {HitPoints}, Position: {Position})";
 
+
+        public void Attack(Creature target)
+        {
+            int damage = TotalDamage();
+
+            _logger.Log(
+                TraceEventType.Information,
+                LogType.Combat,
+                $"{Name} attacks {target.Name} for {damage} damage."
+            );
+            target.ReceiveDamage(damage);
+        }
 
         public void ReceiveDamage(int hitdamage)
         {
@@ -45,6 +59,35 @@ namespace GameFrameworkLibrary.Models.Creatures
                     TraceEventType.Critical,
                     LogType.Combat,
                     $"{Name} has been defeated.");
+            }
+        }
+        public void Move(int dx, int dy, World world)
+        {
+            var from = Position;
+            Position = Position with
+            {
+                X = Math.Clamp(Position.X + dx, 0, world.WorldWidth),
+                Y = Math.Clamp(Position.Y + dy, 0, world.WorldHeight)
+            };
+            _logger.Log(
+                TraceEventType.Information,
+                LogType.World,
+                $"{Name} attempted move from {from} to ({from.X + dx},{from.Y + dy}), clamped to {Position}");
+        }
+
+
+        public void UseItem(WorldObject item)
+        {
+            if (item is IUsable usable)
+            {
+                usable.UseOn(this);
+            }
+            else
+            {
+                _logger.Log(
+                    TraceEventType.Warning,
+                    LogType.Inventory,
+                    $"{Name} tried to use {item.Name} but it is not usable");
             }
         }
 
@@ -68,14 +111,92 @@ namespace GameFrameworkLibrary.Models.Creatures
                 return;
             }
             var loot = source.GetLoot();
-            //EquipLoot(loot);
-            if (container is LootableObject) 
+            EquipLoot(loot);
+            if (container is LootableObject)
             {
                 world.RemoveObject(container);
                 _logger.Log(
                     TraceEventType.Information,
                     LogType.Inventory,
                     $"{Name} looted {container.Name} and removed it from the world");
+            }
+        }
+        private int TotalDamage()
+        {
+            return _attackItems.Count != 0
+            ? _attackItems.Sum(i => i.HitDamage)
+            : 10;
+        }
+        private void AddUsable(IUsable usable)
+        {
+            _usables.Add(usable);
+            _logger.Log(
+                TraceEventType.Information,
+                LogType.Inventory,
+                $"{Name} added usable item to backpack: {((ItemBase)usable).Name}");
+        }
+
+        private void EquipDefenceItem(DefenceItem item)
+        {
+            if (item is not DefenceItem defenceItem)
+            {
+                _logger.Log(
+                    TraceEventType.Warning,
+                    LogType.Inventory,
+                    $"{Name} tried to equip {item.Name} but it is not a defence item");
+                return;
+            }
+            _defenceItems.Add(defenceItem);
+            _logger.Log(
+                TraceEventType.Information,
+                LogType.Inventory,
+                $"{Name} equipped {defenceItem.Name}");
+        }
+        private void EquipAttackItem(AttackItem item)
+        {
+            if (item is not AttackItem attackItem)
+            {
+                _logger.Log(
+                    TraceEventType.Warning,
+                    LogType.Inventory,
+                    $"{Name} tried to equip {item.Name} but it is not an attack item");
+                return;
+            }
+            _attackItems.Add(attackItem);
+            _logger.Log(
+                TraceEventType.Information,
+                LogType.Inventory,
+                $"{Name} equipped {attackItem.Name}");
+        }
+        private void EquipSingleItem(ItemBase item)
+        {
+            switch (item)
+            {
+                case AttackItem attackItem:
+                    EquipAttackItem(attackItem);
+                    break;
+
+                case DefenceItem defenceItem:
+                    EquipDefenceItem(defenceItem);
+                    break;
+
+                case IUsable usable:
+                    AddUsable(usable);
+                    break;
+
+                default:
+                    _logger.Log(
+                        TraceEventType.Warning,
+                        LogType.Inventory,
+                        $"{Name} ignored item '{item.Name}' – unsupported item type.");
+                    break;
+            }
+        }
+        private void EquipLoot(IEnumerable<ItemBase> loot)
+        {
+            foreach (var item in loot)
+            {
+                EquipSingleItem(item);
             }
         }
     }
